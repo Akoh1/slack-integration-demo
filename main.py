817aa63b88a47f367e97cf6b84e4941b4a5cff0a
Akoh1 <akohsamuel018@gmail.com>
2024-05-31 09:38:27 +0100

    - Use env vars to load token - Add requirements.txt

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+from dotenv import load_dotenv
 from interface.user_interface import UserInput
 from interface.user_controller import UserController
 from client.messanger import Slack
@@ -5,17 +7,10 @@ from client.slack_client import SlackWebClient
 
 
 def main():
-    # test_channels = [{"general": "CH172626"}, {"Sam": "CH1234"}]
-    # test_channels = {"general": "CH172626", "Sam": "CH1234"}
-    # print("Select any Channel/User to send a message to")
-    # channels = "\n".join([names for names in test_channels.keys()])
-    # print(channels)
 
-    slack = Slack(
-        SlackWebClient(
-            token="xoxb-7199507593073-7184201220389-cOETYJUglSyr7DGe1WP43NK5"
-        )
-    )
+    load_dotenv()
+
+    slack = Slack(SlackWebClient(token=os.getenv("token")))
     print(slack.get_receivers())
     channels = slack.get_receiver_names()
     channels = "\n".join([name for name in channels])
